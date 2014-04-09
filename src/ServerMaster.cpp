b9b62edd8ca02e2601819c2ffea13fd23fcfbb3f
X1n <crazythat@gmail.com>
2014-04-09 23:08:59 +0800

    X1n 20140409

--- src/ServerMaster.cpp
+++ src/ServerMaster.cpp
@@ -2,6 +2,15 @@
 
 ServerMaster::ServerMaster(int nPlayers, int nCardSets)
 {
+    playerCount = nPlayers;
+    cardSetCount = nCardSets;
+    remainingCards = new CardSet(cardSetCount);
+
+    usedCards = new CardSet[playerCount];//need to initialize for each player in constructor
+    bottomCards = new CardSet(4 * cardSetCount);
+
+    playerScore = new int[playerCount];
+    banker = PLAYER_NONE;
     //randomize the card sets
     //create sockets for each player/worker
     //listen to each sockets
@@ -11,28 +20,54 @@ ServerMaster::ServerMaster(int nPlayers, int nCardSets)
 ServerMaster::~ServerMaster()
 {
     //delete card sets
+    delete remainingCards;
+    delete usedCards;
+    delete bottomCards;
+
     //kill workers
     //close sockets
 }
 
 void ServerMaster::GetScore(int* scoreContainer)
 {
+    scoreContainer = playerScore;
 }
 
 void ServerMaster::Reset()//reset all data
 {
+    remainingCards.Clear();
+    bottomCards.Clear();
+
+    for(int i = 0; i < playerCount; i++)
+    {
+        usedCards[i].Clear();
+    }
+
+    MemSet(playerScore, 0, playerCount);
+    banker = PLAYER_NONE;
 }
 
-void ServerMaster::Shuffle(CardSet cardset)
+void ServerMaster::Shuffle()
 {
+    remainingCards.RandomizeCardSet(cardSetCount);
 }
 
-void ServerMaster::DispatchCard()
+void ServerMaster::DispatchCard(Worker * workers)
 {
+    Card cardToDispatch;
+    int curPlayer = firstPlayer;
+    
+    do
+    {
+        cardToDispatch = remainingCards.GetFirstCard();
+        workers[curPlayer].FetchCard(cardToDispatch);
+        curPlayer = ((curPlayer + 1) == MAX_PLAYER_COUNT) ? PLAYER_1 : (curPlayer + 1);
+    }while(remainingCards.GetCount() > (cardSetCount * 4));
 }
 
 void ServerMaster::ExchangeCard()
 {
+    worker[banker].FetchCard(cardToDispatch)
 }
 
 void ServerMaster::PlayOneRound()
--- src/ServerMaster.h
+++ src/ServerMaster.h
@@ -1,6 +1,17 @@
 #ifndef _SERVER_MASTER_H_
 #define _SERVER_MASTER_H_
 
+typedef enum PLAYER_NAME
+{
+    PLAYER_1,
+    PLAYER_2,
+    PLAYER_3,
+    PLAYER_4,
+    MAX_PLAYER_COUNT,
+    PLAYER_NONE = 0xFF
+}PLAYERNAME;
+
+
 class ServerMaster{
     public: 
         //initialize all needed variables and environment for card in constructor, also need to create workers
@@ -23,9 +34,18 @@ class ServerMaster{
         CardSet remainingCards;
         CardSet* usedCards;//need to initialize for each player in constructor
         CardSet bottomCards;
+
+        int playerCount;
+        int cardSetCount;
+        int levelUpGrade;//20 points or 40 points?
+
         int* playerScore;
-        int banker;
+        int currentRound;
         
+        PLAYERNAME banker;
+        PLAYERNAME firstPlayer;
+        int playingLevel[2];//level0 for PLAYER1/3, level1 for PLAYER2/4
+        Card currentPrime;
 }
 
 
